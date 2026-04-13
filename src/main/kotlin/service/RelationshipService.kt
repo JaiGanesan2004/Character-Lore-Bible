c6753867cdfe2cddc19ee1c58f148654de6a515e
Your Name <your@email.com>
2026-04-13 20:09:47 +0530

    Fifth commit actually haha.  with the inclusion of relationship tables and finally understanding to some extent

--- src/main/kotlin/service/RelationshipService.kt
+++ src/main/kotlin/service/RelationshipService.kt
@@ -2,8 +2,8 @@ import database.CharacterTable
 import database.RelationshipTable
 import model.dtos.RelationAdd
 import model.dtos.RelationResponse
-import model.enums.RelationType
 import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
 import org.jetbrains.exposed.sql.transactions.transaction
 
 object RelationshipService {
@@ -25,19 +25,22 @@ object RelationshipService {
 
     // Get the connections
     fun getRelationships(name: String): List<RelationResponse> = transaction {
-        // 1. Get the Source Character's ID
-        val charId = CharacterTable.select { CharacterTable.name eq name }
+        // 1. Get the Source ID (Zero)
+        val sourceId = CharacterTable.select { CharacterTable.name eq name }
             .singleOrNull()?.get(CharacterTable.id) ?: return@transaction emptyList()
 
-        // 2. Single Query with a specific Join condition
-        // We link RelationshipTable's targetId to CharacterTable's id
-        (RelationshipTable innerJoin CharacterTable)
-            .select { RelationshipTable.characterId eq charId }
-            // The .on is implicit here if the foreign key is defined,
-            // but it's safer to be explicit in complex joins
-            .map { row ->
-                val type = row[RelationshipTable.relationType] // You are using Enum column now
-                val targetName = row[CharacterTable.name] // This is already the Target's name!
+
+        val join = RelationshipTable.innerJoin(
+            otherTable = CharacterTable,
+            onColumn = {targetId},
+            otherColumn = { CharacterTable.id}
+        )
+
+        join.slice(RelationshipTable.relationType, CharacterTable.name)
+            .select { RelationshipTable.characterId eq sourceId }
+            .map{row ->
+                val type = row[RelationshipTable.relationType]
+                val targetName = row[CharacterTable.name]
 
                 RelationResponse(
                     targetName = targetName,
